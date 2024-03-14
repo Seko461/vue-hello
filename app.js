5ce70a2087c4a96ccccd662846d094fc6000ee12
Seko461 <sekopierre@gmail.com>
2024-03-14 20:41:41 +0100

    right data

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            msg: 'Hello Vue!'
+            msg: 'Hello Vue!',
+            image: 'https://picsum.photos/id/237/200/300'
         }
+
     },
 
 }).mount('#app')
